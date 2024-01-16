755bbd665377557f1097e2223f231db97eca9034
teiderrems0 <raoul.teida@facsciences-uy1.cm>
2024-01-16 13:17:51 +0100

    update

--- crossword.c
+++ crossword.c
@@ -731,17 +731,20 @@ void statistique()
         lire(name, USERNAME_SIZE);
         float score;
         printf("------------------------------------------------------------------------------------------------------------------------------------------------------------\n");
-        printf("Sujet\t\t|\t\tNiveau\t\t|\t\tScore\t\t|\tDate\t\t|\tHeure de D%cbut\t\t|\tHeure de Fin\n", 130);
+        // printf("Sujet\t\t|\t\tNiveau\t\t|\t\tScore\t\t|\tDate\t\t|\tHeure de D%cbut\t\t|\tHeure de Fin\n", 130);
+        printf("Sujet\t\t\t\t|Niveau\t\t\t|\t\t\t\tScore\t\t\t|\t\t\t\tDuree(min)\n");
         printf("------------------------------------------------------------------------------------------------------------------------------------------------------------\n");
         while (fscanf(f, "%s %s %s %f %d %d %d %d %d %d %d %d", sub, username, niveau, &score, &d, &m, &h1, &m1, &s1, &h2, &m2, &s2) != EOF)
         {
             if (strcmp(username, name) == 0)
             {
-                printf("\n%s\t\t|%s\t|\t\t%.4f\t\t|\t%d/%d\t\t|\t%d:%d:%d\t\t\t|\t%d:%d:%d\n", sub, niveau, (score * FILE_PATH_SIZE), d, m, h1, m1, s1, h2, m2, s2);
+                // printf("\n%s\t\t|%s\t|\t\t%.4f\t\t|\t%d/%d\t\t|\t%d:%d:%d\t\t\t|\t%d:%d:%d\n", sub, niveau, (score * FILE_PATH_SIZE), d, m, h1, m1, s1, h2, m2, s2);
+                printf("\n%s\t\t\t\t|%s\t|\t\t\t\t%.4f\t\t\t\t|\t\t\t\t%.4f\n", sub, niveau, (score * FILE_PATH_SIZE), duration(h2,m2,s2,h1,m1,s1));
                 printf("------------------------------------------------------------------------------------------------------------------------------------------------------------\n");
             }
         }
         fclose(f);
+        return;
     }
     printf("\nAucune statistique disponible.\n");
     return;
@@ -765,4 +768,10 @@ void appropos()
     printf("\n2-Education qui met en vigueur votre culture informatique.\n");
     printf("\n3-medecine qui met en vigueur votre culture dans le domaine de la medecine.\n");
     char c=getchar();
+}
+
+float duration(int h1,int m1,int s1,int h2,int m2,int s2)
+{
+    float tim=((float)(h1*60)+(float)m1 +((float)(s1)/(float)60))-((float)(h2*60)+(float)m2 +((float)(s2)/(float)60));
+    return tim;
 }
